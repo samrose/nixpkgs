a5ea12e68d33ca96cfd6a415be6be905d1bdaee8
samrose <samuel.rose@gmail.com>
2019-02-12 10:58:35 -0500

    Revert "adding patch attempt to map serial2 to uart2 in dts"
    This reverts commit 7e989d736dddb9045657c0f2f9787d558ec71329.

--- pkgs/os-specific/linux/kernel/linux-testing.nix
+++ pkgs/os-specific/linux/kernel/linux-testing.nix
@@ -9,10 +9,7 @@ buildLinux (args // rec {
     url = "https://git.kernel.org/torvalds/t/linux-${version}.tar.gz";
     sha256 = "0qga2x4rz1vphi7j044f4b4la24qyk5sm7lm8q991imq5wm5s2rl";
   };
-  kernelPatches = [{
-    patch = ./uart2.patch;
-    name = "uart2";
-  }];
+
   # Should the testing kernels ever be built on Hydra?
   extraMeta.hydraPlatforms = [];
 
